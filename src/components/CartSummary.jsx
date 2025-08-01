db9a935d8c500603fb21b870ad5a2cf6965095e0
HariPriya06-N <natukulaharipriya@gmail.com>
2025-08-01 14:01:56 +0530

    initial commit

--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -23,7 +23,7 @@ const CartSummary = () => {
   const handlePostPayment = async () => {
     try {
       const message = generateCartMessage();
-      await axios.post('http://localhost:5001/send-whatsapp', { message });
+      await axios.post('https://ecomme-backend-production.up.railway.app/send-whatsapp', { message });
       setWhatsappStatus('✅ Message sent!');
     } catch (err) {
       console.error("WhatsApp Error:", err);
